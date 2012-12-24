66fdc948849d74f52535f91956cf3ed23985539f
Artur Burtsev <artjock@gmail.com>
2012-12-24 15:25:59 +0400

    formula

--- test/spec/formula.js
+++ test/spec/formula.js
@@ -1,5 +1,7 @@
 var expect = require('expect.js');
 var vars = require('../../lib/vars.js');
+var Factor = require('../../lib/factor.js');
+
 var Formula = process.env.COVERAGE ?
     require('../../lib-cov/formula.js') :
     require('../../lib/formula.js');
@@ -11,41 +13,49 @@ describe('formula', function() {
         var fBina = [{type: vars.FACTORTYPE.BINARY}, {type: vars.FACTORTYPE.BINARY}];
         var fFilt = [{type: vars.FACTORTYPE.FILTER}, {type: vars.FACTORTYPE.FILTER}];
 
-        it('should initialize properties', function() {
-            var formula = new Formula();
-            expect(formula.factors.filter).to.eql([]);
-            expect(formula.factors.binary).to.eql([]);
-            expect(formula.factors.rank).to.eql([]);
+        it('should save factors', function() {
+            var formula = new Formula([1,2,3]);
+            expect(formula.factors).to.eql([1,2,3]);
         });
+    });
 
-        it('should add filtering factors', function() {
-            var formula = new Formula(fFilt);
-            expect(formula.factors.filter).to.eql(fFilt);
-            expect(formula.factors.binary).to.eql([]);
-            expect(formula.factors.rank).to.eql([]);
-        });
+    describe('exec', function() {
 
-        it('should add ranking factors', function() {
-            var formula = new Formula(fRank);
-            expect(formula.factors.filter).to.eql([]);
-            expect(formula.factors.binary).to.eql([]);
-            expect(formula.factors.rank).to.eql(fRank);
-        });
+        it('should fit ranks in array', function() {
+            var formula = new Formula([
+                {exec: function() {return [1,2,3,0];} },
+                {exec: function() {return [-1,0,0,-1];} },
+                {exec: function() {return [0,3,3,0];} },
+            ]);
 
-        it('should add binary factors', function() {
-            var formula = new Formula(fBina);
-            expect(formula.factors.filter).to.eql([]);
-            expect(formula.factors.binary).to.eql(fBina);
-            expect(formula.factors.rank).to.eql([]);
-        });
-
-        it('should add all factors', function() {
-            var formula = new Formula([].concat(fRank, fBina, fFilt));
-            expect(formula.factors.filter).to.eql(fFilt);
-            expect(formula.factors.binary).to.eql(fBina);
-            expect(formula.factors.rank).to.eql(fRank);
+            expect(formula.exec([], {})).to.eql([[1,2,3,0], [-1,0,0,-1], [0,3,3,0]]);
         });
 
     });
 
+    describe('merge', function() {
+
+        it('should summary all the ranks', function() {
+            var formula = new Formula([]);
+            expect(formula.merge([[1,2,3,0], [-1,0,0,-1], [0,3,3,0]]))
+                .to.eql([-1,5,6,-1]);
+        });
+
+    });
+
+    describe('compile', function() {
+
+        it('should sort array of numbers according to the summary', function() {
+            var formula = new Formula([]);
+
+            expect(formula.compile([1,2,3,4], [-1,5,6,-1])).to.eql([3,2]);
+        });
+
+        it('should sort array of objects according to the summary', function() {
+            var formula = new Formula([]);
+
+            expect(formula.compile([{a:1},{a:2},{a:3},{a:4}], [-1,5,6,-1])).to.eql([{a:3},{a:2}]);
+        });
+
+    });
 });
