12164a109c8200d8dee7172ba1ce8b2aee68f3d5
Artur Burtsev <artjock@gmail.com>
2013-01-15 18:08:13 +0400

    factor mocks

---
+++ test/mock/factor.js
@@ -0,0 +1,53 @@
+var vars = require('../../lib/vars.js');
+var Factor = require('../../lib/factor.js');
+
+var data = {
+
+    'sort plain': {
+        run: function(data) {
+            return data.sort();
+        }
+    },
+
+    'sort obj': {
+        key: function(item) {
+            return item.i;
+        },
+        run: function(data) {
+            return data.sort(function(a, b) {
+                return a.i - b.i;
+            })
+        }
+    },
+
+    'only odd': {
+        type: vars.FACTORTYPE.BINARY,
+        run: function(data) {
+            return data.map(function(item) {
+                return item % 2 ? true : false;
+            });
+        }
+    },
+
+    'gt3': {
+        type: vars.FACTORTYPE.FILTER,
+        run: function(data) {
+            return data.filter(function(item) {
+                return item > 3;
+            });
+        }
+    },
+
+    'lt5': {
+        type: vars.FACTORTYPE.FILTER,
+        run: function(data) {
+            return data.filter(function(item) {
+                return item < 5;
+            });
+        }
+    }
+};
+
+module.exports = function(name) {
+    return new Factor( data[name] );
+};
--- test/spec/factor.js
+++ test/spec/factor.js
@@ -5,61 +5,22 @@ var Factor = process.env.COVERAGE ?
     require('../../lib-cov/factor.js') :
     require('../../lib/factor.js');
 
+var mock = require('../mock/factor');
+
 describe('factor', function() {
 
     beforeEach(function() {
 
         this.dataPlain = [4,0,2,1];
         this.dataObj = [{i:4}, {i:0}, {i:2}, {i:1}];
+        this.factors = [
+            mock('sort plain'),
+            mock('sort obj'),
+            mock('only odd'),
+            mock('gt3'),
+            mock('lt5')
+        ];
 
-        this.sortFactorPlain = new Factor({
-            name: 'sort plain',
-            run: function(data) {
-                return data.sort();
-            }
-        });
-
-        this.sortFactorObj = new Factor({
-            name: 'sort obj',
-            key: function(item) {
-                return item.i;
-            },
-            run: function(data) {
-                return data.sort(function(a, b) {
-                    return a.i - b.i;
-                })
-            }
-        });
-
-        this.oddFactorPlain = new Factor({
-            name: 'only odd',
-            type: vars.FACTORTYPE.BINARY,
-            run: function(data) {
-                return data.map(function(item) {
-                    return item % 2 ? true : false;
-                });
-            }
-        });
-
-        this.gt3FactorPlain = new Factor({
-            name: 'only odd',
-            type: vars.FACTORTYPE.FILTER,
-            run: function(data) {
-                return data.filter(function(item) {
-                    return item > 3;
-                });
-            }
-        });
-
-        this.lt5FactorPlain = new Factor({
-            name: 'only odd',
-            type: vars.FACTORTYPE.FILTER,
-            run: function(data) {
-                return data.filter(function(item) {
-                    return item < 5;
-                });
-            }
-        });
     });
 
     describe('constructor', function() {
@@ -96,7 +57,7 @@ describe('factor', function() {
     describe('exec', function() {
 
         it('should not modify original data while ranking', function() {
-            this.sortFactorPlain.exec(this.dataPlain);
+            this.factors[0].exec(this.dataPlain);
             expect(this.dataPlain).to.eql([4,0,2,1]);
         });
 
@@ -106,31 +67,31 @@ describe('factor', function() {
     describe('distribution', function() {
 
         it('should properly distribute rank', function() {
-            this.sortFactorPlain.exec(this.dataPlain).then(function(data) {
+            this.factors[0].exec(this.dataPlain).then(function(data) {
                 expect(data).to.eql([0,3,1,2]);
             });
         });
 
         it('should properly distribute rank on objects', function() {
-            this.sortFactorObj.exec(this.dataObj).then(function(data) {
+            this.factors[1].exec(this.dataObj).then(function(data) {
                 expect(data).to.eql([0,3,1,2]);
             });
         });
 
         it('should append maximum rank to binary trues', function() {
-            this.oddFactorPlain.exec(this.dataPlain).then(function(data) {
+            this.factors[2].exec(this.dataPlain).then(function(data) {
                 expect(data).to.eql([0,0,0,3]);
             });
         });
 
         it('should pass by filters', function() {
-            this.gt3FactorPlain.exec(this.dataPlain).then(function(data) {
+            this.factors[3].exec(this.dataPlain).then(function(data) {
                 expect(data).to.eql([0,-1,-1,-1]);
             });
         });
 
         it('should return [] when nothing were filtered', function() {
-            this.lt5FactorPlain.exec(this.dataPlain).then(function(data) {
+            this.factors[4].exec(this.dataPlain).then(function(data) {
                 expect(data).to.eql([]);
             });
         });
