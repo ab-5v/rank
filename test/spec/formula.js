4ee0755d430598f1430fb96da2c80741da704045
Artur Burtsev <artjock@gmail.com>
2013-02-25 16:50:27 +0400

    new formula

--- test/spec/formula.js
+++ test/spec/formula.js
@@ -1,108 +1,123 @@
+var sinon = require('sinon');
 var expect = require('expect.js');
+var pzero = require('p0');
 
-var Formula = process.env.COVERAGE ?
+var factor = require('../../lib/factor.js');
+
+var formula = process.env.COVERAGE ?
     require('../../lib-cov/formula.js') :
     require('../../lib/formula.js');
 
-describe('formula', function() {
+describe.only('formula', function() {
 
-    describe('constructor', function() {
+    beforeEach(function() {
 
-        it('should save factors', function() {
-            var formula = new Formula([1,2,3]);
-            expect(formula.factors).to.eql([1,2,3]);
+        var data = this.data = [1, 2, 3];
+
+        this.f1 = factor({value: function() {}});
+        this.f2 = factor({value: function() {}});
+        this.f3 = factor({valueAll: function() { return data; }});
+
+        sinon.spy(this.f1, 'append');
+        sinon.spy(this.f2, 'append');
+        sinon.spy(this.f3, 'append');
+
+        this.formula = formula([this.f1, this.f2, this.f3]);
+
+        sinon.spy(this.formula, 'exec');
+        sinon.spy(this.formula, 'compile');
+    });
+
+
+    describe('_create', function() {
+
+        it('should save factors to `_factors`', function() {
+            expect( this.formula._factors ).to.eql( [this.f1, this.f2, this.f3] );
         });
 
-    });
-
-    describe('distribution', function() {
-    });
-
-    describe('limits', function() {
-        it('should set limits for filter factor', function() {});
-        it('should set limits for binary factor', function() {});
-        it('should set limits for sort factor', function() {});
-        it('should set limits for minmax factor', function() {});
-        it('should increase max, when min=max', function() {});
-    });
-
-    describe('stat', function() {
-
-        beforeEach(function() {
-            this.formula = new Formula([]);
+        it('should save all simple factors to `_simples`', function() {
+            expect( this.formula._simples ).to.eql( [this.f1, this.f2] );
         });
 
-        it('should return stat', function() {
-            expect( this.formula.stat( [[1,2,3], [3,2,1], [-1,0,0], [3,0,3]] ) )
-                .to.eql( [[1,3,-1,3], [2,2,0,0], [3,1,0,3]] );
+        it('should create array `_weights` with length of factors.length', function() {
+            expect( this.formula._weights.length ).to.be.eql( 3 );
         });
 
     });
 
     describe('weight', function() {
 
-        it('should calculate partially defined weight', function() {
-            var formula = new Formula([{}, {}, {}]);
+        it('should fill with "1" by default on undefiend', function() {
+            expect( this.formula.weight() ).to.eql( [1, 1, 1] );
+        });
 
-            expect( formula.weight([0.5, 0.2]) )
-                .to.eql([0.5, 0.2, 1]);
+        it('should fill with "1" by default on empty array', function() {
+            expect( this.formula.weight([]) ).to.eql([1, 1, 1]);
+        });
+
+        it('should append "1" when weights.length < factors.length', function() {
+            expect( this.formula.weight([2, 3]) ).to.eql( [2, 3, 1] );
+        });
+
+        it('should cut when weights.length > factors.length', function() {
+            expect( this.formula.weight([2, 3, 4, 5]) ).to.eql( [2, 3, 4] );
         });
 
     });
 
     describe('exec', function() {
 
-        it('should fit ranks in array', function() {
-            var formula = new Formula([
-                {exec: function() {return [1,2,3,0];} },
-                {exec: function() {return [-1,0,0,-1];} },
-                {exec: function() {return [0,3,3,0];} },
-            ]);
+        it('should return array of promises for all factors', function() {
+            var promises = this.formula.exec(this.data, {})
+                .filter(function(promise) { return pzero.is(promise); });
 
-            expect(formula.exec([], {})).to.eql([[1,2,3,0], [-1,0,0,-1], [0,3,3,0]]);
+            expect( promises.length ).to.eql(3);
         });
 
-    });
-
-    describe('merge', function() {
-
-        it('should summary all the ranks', function() {
-            var formula = new Formula([{}, {}, {}]);
-            expect(formula.merge([[1,2,3,0], [-1,0,0,-1], [0,3,3,0]], [1,1,1]))
-                .to.eql([-1,5,6,-1]);
-        });
-
-        it('should use weights', function() {
-            var formula = new Formula([{},{},{}]);
-
-            expect(formula.merge([[-1, 0, -1, 0], [1, 2, 4, 5], [4, 6, 7, 8]], [1, 0.5, 0]))
-                .to.eql([-1, 1, -1, 2.5]);
-        });
 
     });
 
     describe('compile', function() {
 
-        it('should sort array of numbers according to the summary', function() {
-            var formula = new Formula([]);
+        it('should compile values', function() {});
 
-            expect(formula.compile([1,2,3,4], [-1,5,6,-1], [[1], [2], [3], [4]]))
-                .to.eql({ result: [3,2], stat: [[3], [2]]});
+    });
+
+    describe('run', function() {
+
+        it('should call "append" on each simple factor', function() {
+            this.formula.run(this.data, {});
+
+            expect( this.f1.append.called ).to.be.ok();
+            expect( this.f2.append.called ).to.be.ok();
         });
 
-        it('should sort array of objects according to the summary', function() {
-            var formula = new Formula([]);
+        it('should iterate through all data and call "append" with index, item and cond', function() {
+            var cond = {a: 1};
+            this.formula.run(this.data, cond);
+            var args = this.f1.append.args;
 
-            expect(formula.compile([{a:1},{a:2},{a:3},{a:4}], [-1,5,6,-1], [[1], [2], [3], [4]]))
-                .to.eql({ result: [{a:3},{a:2}], stat: [[3], [2]]});
+            expect( args[0] ).to.eql( [0, 1, cond] );
+            expect( args[1] ).to.eql( [1, 2, cond] );
+            expect( args[2] ).to.eql( [2, 3, cond] );
         });
 
-        it('should return different values on equal ranks', function() {
-            var formula = new Formula([]);
+        it('should not call "append" on "isAll" factors', function() {
+            this.formula.run(this.data, {});
 
-            expect(formula.compile([1,2,3,4], [-1,5,5,-1], [[1], [2], [3], [4]]))
-                .to.eql({ result: [2,3], stat: [[2], [3]] });
+            expect( this.f3.append.called ).not.to.be.ok();
+        });
+
+        it('should call "exec" and "compile"', function(done) {
+            this.formula.run(this.data, {});
+
+            setTimeout(function() {
+                expect( this.formula.exec.called ).to.be.ok();
+                expect( this.formula.compile.called ).to.be.ok();
+                done();
+            }.bind(this), 20);
         });
 
     });
+
 });
